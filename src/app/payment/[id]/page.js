d92a07deb45ed599ecb9787c9cff8f6bf4fef828
Jaxx1911 <truonghoangnguyen1911@gmail.com>
2025-05-29 21:09:08 +0700

    fix ws

--- src/app/payment/[id]/page.js
+++ src/app/payment/[id]/page.js
@@ -57,7 +57,7 @@ export default function PaymentPage() {
             })
         }, 1000)
 
-        const ws = new WebSocket(NEXT_PUBLIC_WEBSOCKET_URL + '?t=' + 'Bearer ' + localStorage.getItem('access_token') + '&r=payment')
+        const ws = new WebSocket(process.env.NEXT_PUBLIC_WEBSOCKET_URL + '?t=' + 'Bearer ' + localStorage.getItem('access_token') + '&r=payment')
 
         ws.onopen = () => {
             console.log('WebSocket Connected')
